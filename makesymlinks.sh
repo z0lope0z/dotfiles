0e8419812c5ac88419b183ba624d7008a474b0b2
lope <lope@lopegwapo@gmail.com>
2014-02-14 11:17:44 +0800

    updated list, added gitconfig

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh tmux.conf xmonad"    # list of files/folders to symlink in homedir
+files="bashrc zshrc tmux.conf xmonad oh-my-zsh gitconfig" # list of files/folders to symlink in homedir
 
 ##########
 
@@ -56,4 +56,4 @@ else
 fi
 }
 
-install_zsh
+# install_zsh
